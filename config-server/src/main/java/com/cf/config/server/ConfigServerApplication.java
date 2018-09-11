0eda64254756e2bf92a5f9f302785371abe68ef3
“martin” <martinchen911@outlook.com>
2018-09-11 13:53:53 +0800

    分布式配置中心&客户端

---
+++ config-server/src/main/java/com/cf/config/server/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.cf.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @EnableConfigServer 开启配置服务注解
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
