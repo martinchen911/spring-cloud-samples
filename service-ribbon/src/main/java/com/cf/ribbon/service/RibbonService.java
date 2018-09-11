51d71a45a2a2810a5dfb96cd69bdd2326a3c326e
“martin” <martinchen911@outlook.com>
2018-09-11 10:55:22 +0800

    断路器（Hystrix）-- ribbon中使用

--- service-ribbon/src/main/java/com/cf/ribbon/service/RibbonService.java
+++ service-ribbon/src/main/java/com/cf/ribbon/service/RibbonService.java
@@ -1,5 +1,6 @@
 package com.cf.ribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,7 +11,13 @@ public class RibbonService {
     @Autowired
     RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "sayHiError")
     public String sayHi(String name) {
         return restTemplate.getForObject("http://SERVICE-PROVIDER/hi?name="+name,String.class);
     }
+
+    public String sayHiError(String name) {
+        return "hi,"+name+",sorry,error!";
+    }
+
 }
