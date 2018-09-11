b4a6c7f4b6586c95d4606d0442fa760ead64cbbc
“martin” <martinchen911@outlook.com>
2018-09-11 14:17:43 +0800

    eureka-server&config-server&config-client -- 构建高可用的分布式配置中心

--- config-server/src/main/java/com/cf/config/server/ConfigServerApplication.java
+++ config-server/src/main/java/com/cf/config/server/ConfigServerApplication.java
@@ -3,10 +3,12 @@ package com.cf.config.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @EnableConfigServer 开启配置服务注解
  */
+@EnableEurekaClient
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
