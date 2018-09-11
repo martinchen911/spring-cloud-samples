5261f2fc167d62ebbeb32bee97125d88add8a873
“martin” <martinchen911@outlook.com>
2018-09-11 11:01:52 +0800

    断路器（Hystrix）-- Feign中使用

---
+++ service-feign/src/main/java/com/cf/feign/config/FeignServiceHystric.java
@@ -0,0 +1,12 @@
+package com.cf.feign.config;
+
+import com.cf.feign.service.FeignService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FeignServiceHystric implements FeignService {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+}
--- service-feign/src/main/java/com/cf/feign/service/FeignService.java
+++ service-feign/src/main/java/com/cf/feign/service/FeignService.java
@@ -1,10 +1,11 @@
 package com.cf.feign.service;
 
+import com.cf.feign.config.FeignServiceHystric;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "service-provider")
+@FeignClient(value = "service-provider",fallback = FeignServiceHystric.class)
 public interface FeignService {
 
     @GetMapping("/hi")
