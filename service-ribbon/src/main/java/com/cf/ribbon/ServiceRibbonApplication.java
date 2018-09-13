3a78fb98ecadfb778aad6df56b6435a906376ae2
“martin” <martinchen911@outlook.com>
2018-09-13 19:14:26 +0800

    添加注释信息

--- service-ribbon/src/main/java/com/cf/ribbon/ServiceRibbonApplication.java
+++ service-ribbon/src/main/java/com/cf/ribbon/ServiceRibbonApplication.java
@@ -3,15 +3,14 @@ package com.cf.ribbon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
-
+//开启断路器
 @EnableHystrix
+//开启Eureka客户端
 @EnableEurekaClient
+//开启服务发现
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ServiceRibbonApplication {
@@ -20,9 +19,5 @@ public class ServiceRibbonApplication {
         SpringApplication.run(ServiceRibbonApplication.class, args);
     }
 
-    @Bean
-    @LoadBalanced
-    RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+
 }
